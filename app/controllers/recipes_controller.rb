abc5625448a170e88987b975c17ad91ce895932a
Hannah Malm <hannahemalm616@gmail.com>
2022-07-24 12:07:57 -0500

    added in controllers

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,14 @@
 class RecipesController < ApplicationController
+
+
+    #show all drinks
+    def index  #just want the logs associted with a specific type of run 
+        render json: @recipes
+    end 
+
+    #show a specific drink
+    def show 
+        @recipe = Recipe.find(params[:id])
+        render json: @recipe
+    end 
 end
