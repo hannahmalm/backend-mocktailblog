91caea7d23b4f81b76fde110b3d7ae5b04450855
Hannah Malm <hannahemalm616@gmail.com>
2022-07-24 11:22:38 -0500

    updated models for recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,7 @@
 class Recipe < ApplicationRecord
-    
+    validates :name, presence: true
+    # validates :image, presence: true
+    # validates :description, presence: true
+    # validates :ingredient, presence: true
+    # validates :directions, presence: true
 end
